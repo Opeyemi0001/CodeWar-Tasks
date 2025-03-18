ccf878ca12fbc3320401eb1fba81be0e001f9de1
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-03-19 00:19:21 +0100

    added new task

--- script.js
+++ script.js
@@ -394,3 +394,40 @@ function makeNegative(num) {
 }
 
 console.log(makeNegative(-45))
+
+
+
+// It's pretty straightforward. 
+// Your goal is to create a function that removes the first and last characters
+// of a string. You're given one parameter, the original string. You don't have
+// to worry about strings with less than two characters.
+
+function removeChar(str){
+  return str.slice(1, -1);
+}
+console.log(removeChar("hello"))
+
+
+// Write a function that removes the spaces from the string, 
+// then return the resultant string.
+
+// Examples (Input -> Output):
+
+// "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+// "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+// "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+function noSpace(x) {
+  return x.replace(/\s/g, "");
+}
+console.log(noSpace("jshdh sunwn hsjns"));
+
+
+
+// Complete the method that takes a boolean value and return a 
+// "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  return bool ? "Yes" : "No";
+}
+console.log(boolToWord(false))

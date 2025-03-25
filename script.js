426d74f6a35b0df5b7eec6b59bff7573fb7d54b9
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-03-25 12:58:43 +0100

    feat: added task

--- script.js
+++ script.js
@@ -472,3 +472,81 @@ function basicOp(operation, value1, value2) {
   }
 }
 console.log(basicOp("/", 10, 5));
+
+
+
+// Your task is to write a function which returns the n-th term of the following series, 
+// which is the sum of the first n terms of the sequence (n is the input parameter).
+
+// Series: 1+ 1/4 + 1/7 + 1/10 + 1/13 + 1/16 + …
+
+// You will need to figure out the rule of the series to complete this.
+
+// Rules
+// You need to round the answer to 2 decimal places and return it as String.
+// If the given value is 0 then it should return "0.00".
+// You will only be given Natural Numbers as arguments.
+// Examples (Input --> Output)
+// n
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n) {
+  let sum = 0;
+  let denominator = 1;
+  for (let i = 0; i < n; i++) {
+    sum += 1 / denominator;
+    denominator += 3;
+  }
+  return sum.toFixed(2);
+}
+console.log(SeriesSum(2));
+
+
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, 
+// he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to 
+// return the number of litres Nathan will drink, rounded down.
+
+// For example:
+// time = 3 ----> litres = 1
+// time = 6.7---> litres = 3
+// time = 11.8--> litres = 5
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+console.log(litres(6.7));
+
+
+// The first century spans from the year 1 up to and including the year 100, 
+// the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+// 2742 --> 28
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
+console.log(century(1900));
+
+
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x) {
+  return x.map((num)=> num * 2);
+}
+console.log(maps([1, 5, 3]));

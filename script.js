e68ecc47c881cf67674727e9f043d68741b4529f
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-04-17 20:26:36 +0100

    added task

--- script.js
+++ script.js
@@ -801,3 +801,62 @@ const dog2 = new Dog("Luna", "Labrador", "female", 2);
 
 console.log(dog1.bark()); // "Woof!"
 console.log(dog2.bark());
+
+
+
+
+// (47) Your task is to add a new property usersAnswer to every object in the array questions.
+// The value of usersAnswer should be set to null. The solution should work for array of any length.
+
+// For example:
+
+var questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+// After adding the property the result should be:
+
+// var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }];
+// The questions array is already defined for you and is not the same 
+// as the one in the example.
+
+questions.forEach(function(q) {
+    q.usersAnswer = null;
+});
+
+
+
+// Write a program that finds the summation of every number from 1 to num (both inclusive).
+// The number will always be a positive integer greater than 0. Your function only needs 
+// to return the result, what is shown between parentheses in the example below is
+// how you reach that result and it's not part of it, see the sample tests.
+
+// For example (Input -> Output):
+
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+function summation(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(summation(8));
+

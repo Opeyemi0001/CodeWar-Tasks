e3bf4afd5447e4bb8acd32e84164597e54fe8d87
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-04-22 08:29:38 +0100

    feat: new task

--- script.js
+++ script.js
@@ -882,3 +882,19 @@ function monkeyCount(n) {
 }
 
 console.log(monkeyCount(10));
+
+
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+
+function areYouPlayingBanjo(name ) {
+  return name[0].toLowerCase() === "r" ? `${name} plays banjo` : `${name} does not play banjo`;
+}
+console.log(areYouPlayingBanjo("Rita"));

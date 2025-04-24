c083497b492ab60d6ec1d621c454b4f2341bc34b
opeyemi0001 <opeyemionanuga40@gmail.com>
2025-04-24 18:28:29 +0100

    feat: task updateed

--- script.js
+++ script.js
@@ -898,3 +898,24 @@ function areYouPlayingBanjo(name ) {
   return name[0].toLowerCase() === "r" ? `${name} plays banjo` : `${name} does not play banjo`;
 }
 console.log(areYouPlayingBanjo("Rita"));
+
+
+// Write a function that takes a single non-empty string of only lowercase 
+// and uppercase ascii letters (word) as its argument, and returns an ordered 
+// list containing the indices of all capital (uppercase) letters in the string.
+
+// Example (Input --> Output)
+// "CodEWaRs" --> [0,3,4,6]
+
+
+function capitals(word) {
+  const indices = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      indices.push(i);
+    }
+  }
+  return indices;
+}
+
+console.log(capitals("CodEWaRs"));
